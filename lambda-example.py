11be8279446e4893d9b6c523fbc77da7fa90ba69
ChibisovEB <124187608+ChibisovEB@users.noreply.github.com>
2023-06-10 15:48:11 +0500

    Update lambda-example.py - lambda into lambda

--- lambda-example.py
+++ lambda-example.py
@@ -24,3 +24,12 @@ for table in tables:
 max_number = lambda a, b : a if a > b else b
 print(f"выводим максмальное число {max_number(30, 102)}")
 print(a if a > b else b) # вывод условный
+
+# Лямбда и множественные операторы
+# Вложенная лямбда функция
+current_list = [[10,6,9],[0, 14, 16, 80],[8, 12, 30, 44]]
+sorted_list = lambda x: (sorted(i) for i in x)
+second_largest = lambda x, func: [y[len(y)-2] for y in func(x)]
+result = second_largest(current_list, sorted_list)
+print(current_list)
+print(result)
