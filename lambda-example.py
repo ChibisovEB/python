257a8205e606220e812ab3f61b043c7d25ab9bd2
ChibisovEB <124187608+ChibisovEB@users.noreply.github.com>
2023-06-10 10:37:04 +0500

    Update lambda-example.py

--- lambda-example.py
+++ lambda-example.py
@@ -11,8 +11,8 @@ myList = [1,2,3,4,5, "weecg",7,8,90,23,5,4,6, "were",78,66,67,22,23,45,43,44]
 # print(f'Sum of {a} and {b} is {sum(a, b)} with tradition function')
 # print(f'Sum of {a} and {b} is {ll} with lambda function')
 print(f'MyList  {myList}')
-myList = list(filter(lambda x: type(x) is int , myList))
-print(f'Четные {list(filter(lambda x: (x%2 == 0) , myList))}')
-print(f'Нечетные {list(filter(lambda x: (x%2 != 0) , myList))}')
-print(f'Конвертируем в строки {list(map(lambda x: (str(x)) , myList))}')
-print(f'reduce {reduce((lambda x, y: x + y), myList)}')
+myList = list(filter(lambda x: type(x) is int , myList)) # исключаем из списка все нечисла
+print(f'Четные {list(filter(lambda x: (x%2 == 0) , myList))}') # показываем только четные числа
+print(f'Нечетные {list(filter(lambda x: (x%2 != 0) , myList))}') # показываем только четные нечисла
+print(f'Конвертируем в строки {list(map(lambda x: (str(x)) , myList))}') # конвертация в строки всего спсика
+print(f'reduce {reduce((lambda x, y: x + y), myList)}') # сумма всех элементов
