21f6fae05f611c8aa875f8d8e78080d9cd9b3efc
Landgraf <e4ibisoff@ya.ru>
2023-06-13 00:07:49 +0700

    update

--- HomeWork7.py
+++ HomeWork7.py
@@ -64,8 +64,7 @@ else:
 
 def print_operation_table(operation,num_rows,num_columns):   
     for row in range(1,num_rows+1):
-        for col in range(1,num_columns+1):
-            print(operation(row,col), end="\t")
+        for col in range(1,num_columns+1): print(operation(row,col), end="\t")
         print("\n")
 
 print_operation_table(lambda x, y: x * y,16,8)
