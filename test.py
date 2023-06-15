544cf1e2688aebd4d194973b185f983d0815e900
ChibisovEB <124187608+ChibisovEB@users.noreply.github.com>
2023-06-15 15:29:17 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
+import os
+import platform 
+print(os.name)
+print(os.getcwd())
+print(os.listdir(path=os.getcwd()))
 
+print("=========================") 
+print(platform.system())
+print(platform.node())
+print(platform.platform())
+print(platform.processor())
+print(platform.python_version())
+print(platform.version())
+print(platform.uname())
+
+
+for current_dir, dirs, files in os.walk("."):
+    print(current_dir, dirs, files)
