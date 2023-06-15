e16a32f523ca331100843a0a35112cd3019a7427
ChibisovEB <124187608+ChibisovEB@users.noreply.github.com>
2023-06-15 10:30:53 +0500

    Update HomeWork8.py

--- HomeWork8.py
+++ HomeWork8.py
@@ -28,15 +28,6 @@ def choose_action(phonebook):
             continue
 
 
-def import_data(file_to_add, phonebook):
-    try:
-        with open(file_to_add, 'r', encoding='utf-8') as new_contacts, open(phonebook, 'a', encoding='utf-8') as file:
-            contacts_to_add = new_contacts.readlines()
-            file.writelines(contacts_to_add)
-    except FileNotFoundError:
-        print(f'{file_to_add} не найден')
-
-
 def read_file_to_dict(file_name):
     with open(file_name, 'r', encoding='utf-8') as file:
         lines = file.readlines()
