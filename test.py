84dcdc6fd15560ba6b51e4b38617518df36c1c6f
Landgraf <e4ibisoff@ya.ru>
2023-06-26 16:04:37 +0700

    testv

--- test.py
+++ test.py
@@ -8,10 +8,10 @@ random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 print(data)
 
-==================================================
 data['tmp'] = 1
 data.set_index([data.index, 'whoAmI'], inplace=True)
 data = data.unstack(level=-1, fill_value = 0).astype(int)
 data.columns = data.columns.droplevel()
 data.columns.name = None
-data.head(20)
+# data.head(20)
+print(data)
